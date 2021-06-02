0eb6e237026bf3a9b3925754c8b9410e7a4605f1
Sandeep Nuka <sandeepnuka21@gmail.com>
2021-06-02 21:06:24 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends React.Component {
 
     if (city && country) {
       const api_call = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Apikey}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Apikey}`
       );
       const responce = await api_call.json();
       console.log(responce);
